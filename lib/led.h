c090965149121c70c6711b3cc4e3a9ec057b6ca7
mikuriya <mickly9@hotmail.com>
2019-06-27 17:31:15 +0900

    たぶんできた

--- lib/led.h
+++ lib/led.h
@@ -1,22 +1,22 @@
 #pragma once
 
-#define LED_WIDTH  16
+#define LED_WIDTH 16
 #define LED_HEIGHT 32
-#define LED_DEPTH  8
+#define LED_DEPTH 8
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
 /*!
  Specify the URL of 3D LED CUBE.
- If you specify a URL, the drawing data is transferred each time you call the Show function.
+ If you specify a URL, the drawing data is transferred each time you call the
+ Show function.
  @param[in] url 3D LED CUBE IP ADDRESS
+ @param[in] port 3D LED CUBE PORT
+ @return 0: success
 */
-void SetUrl(char const * url);
-
-/*!
-Specify the PORT of 3D LED CUBE.
-If you specify a URL, the drawing data is transferred each time you call the Show function.
-@param[in] port 3D LED CUBE PORT
-*/
-void SetPort(unsigned short port);
+int InitSdk(char const* url, unsigned short port);
 
 /*!
  Specify the color of each LED.
@@ -35,10 +35,10 @@ void SetLed(int x, int y, int z, int rgb);
 void Clear();
 
 /*!
- Show 3D LED simulator window and transfer color of each LED to 3D LED CUBE.
+ Send packet to 3D LED
+ @return 0: success
 */
-void Show();
-
+int Show();
 
 /*!
  Wait for the specified time.
@@ -47,36 +47,6 @@ void Show();
 */
 void Wait(int ms);
 
-/*!
- Enable or Disable a Simulator Window.
- Default setting is Enable.
- @param[in] isEnable true: Show Simulator Window, false: Hide
- */
-void EnableSimulator(bool isEnable);
-
-/*!
- Specify the color of each LED for displaying a character.
- Just by calling this function, drawing data is not transferred to 3D LED CUBE.
- @param[in] x 0 <= x < LED_WIDTH
- @param[in] y 0 <= y < LED_HEIGHT
- @param[in] z 0 <= z < LED_DEPTH
- @param[in] c charactor code
- @param[in] rgb 0x000000 <= rgb <= 0xFFFFFF
- */
-void SetChar(int x, int y, int z, char c, int rgb);
-
-/*!
- Display characters one by one
- When the motion ends, it returns from the function.
- @param[in] text displayed charactors
-*/
-void ShowMotioningText1(const char * text);
-
-/*!
- Display a firework.
- When the motion ends, it returns from the function.
- @param[in] x 0 <= x < LED_WIDTH
- @param[in] y 0 <= y < LED_HEIGHT
- @param[in] z 0 <= z < LED_DEPTH
- */
-void ShowFirework(int x, int y, int z);
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
